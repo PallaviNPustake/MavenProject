98d49bcaa9751cd04f5d7a44582ec785e75bdf60
PallaviNPustake <pallavinpustake@gmail.com>
2023-07-06 11:48:00 +0530

    [PallaviP] This is my initial commit, created test1 class.

---
+++ src/main/java/mainP/Test1.java
@@ -0,0 +1,20 @@
+package mainP;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+       int marks = 59;
+       if(marks>75)
+       {
+    	   System.out.println("You got distinction");
+       }
+       else
+       {
+    	   System.out.println("You got first class");
+       }
+       
+		
+	}
+
+}
