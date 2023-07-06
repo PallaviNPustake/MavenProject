7fa4b8ef3bfaf37d9bd5d4c6e0775749fd0332dc
SonaliDeshmukh <deshmukhsonali2602@gmail.com>
2023-07-06 16:19:18 +0530

    [Sonali] i just add the one test2 calss in pallavi project

---
+++ src/main/java/mainP/SonaliTest2.java
@@ -0,0 +1,15 @@
+package mainP;
+
+public class SonaliTest2 {
+
+	public static void main(String[] args) 
+	{
+		SonaliTest2.test();
+       
+	}
+	public static void test()
+	{
+		System.out.println("This is my static method");
+	}
+
+}
