8e6c807c1b365ae100052c1b829e61484bb0f25c
PallaviNPustake <pallavinpustake@gmail.com>
2023-07-07 15:11:31 +0530

    [PallaviP] added test4 class.

---
+++ src/main/java/mainP/Test4.java
@@ -0,0 +1,21 @@
+package mainP;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "Pallavi";
+		String s2 = "Pallavi";
+		
+		if(s.equals(s2)) {
+			System.out.println("The string values are matched");
+		}
+		else
+		{
+		System.out.println("Values are mismatched");	
+		}
+		
+	}
+
+}
